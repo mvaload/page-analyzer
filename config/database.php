28ccff51236ba71dbae0af0c528220bf2ce49a28
mvaload <mvaload@gmail.com>
2019-08-23 15:51:26 +0300

    added testing config

--- config/database.php
+++ config/database.php
@@ -79,6 +79,18 @@ return [
             'prefix' => env('DB_PREFIX', ''),
         ],
 
+        'testing' => [
+            'driver'    => 'pgsql',
+            'host'      => env('DB_TEST_HOST', 'localhost'),
+            'database'  => env('DB_TEST_DATABASE', 'homestead_test'),
+            'username'  => env('DB_TEST_USERNAME', 'homestead'),
+            'password'  => env('DB_TEST_PASSWORD', 'secret'),
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
+            'strict'    => false,
+        ],
+
     ],
 
     /*
