60fde7cb9b19d498654f42703e46f05076f93e44
Владимир Моисеенко (33773) <v.moiseenko@navigatorcc.com>
2019-08-26 11:39:10 +0300

    update tests

--- tests/DomainsTest.php
+++ tests/DomainsTest.php
@@ -2,6 +2,14 @@
 
 use Laravel\Lumen\Testing\DatabaseMigrations;
 use Laravel\Lumen\Testing\DatabaseTransactions;
+use App\Http\Controllers\DomainsController;
+use GuzzleHttp\Client;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Psr7\Response;
+use GuzzleHttp\Psr7\Request;
+use GuzzleHttp\Exception\RequestException;
+use App\Post;
 
 class DomainsTest extends TestCase
 {
@@ -9,18 +17,40 @@ class DomainsTest extends TestCase
 
     public function testStore()
     {
-        $this->post('/domains', ['url' => 'https://github.com']);
+        $mockGuzzle = new MockHandler([
+            new Response(200, ['Content-Length' => 0,
+                                'body' => '<h1>Tag</h1>',
+                                'description' => 'description smarty',
+                                'keywords' => 'keywords smarty']),
+            new Response(200, ['body' => '<h1>Tag</h1>',
+                               'description' => 'description smarty',
+                               'keywords' => 'keywords smarty'])
+        ]);
+        $handler = HandlerStack::create($mockGuzzle);
+        $guzzle = new Client(['handler' => $handler]);
+        $this->app->instance(Client::class, $guzzle);
+
+        $this->post(route('domains.store'), ['url' => 'https://www.smarty.net']);
         $this->seeStatusCode(302);
-        $this->seeInDatabase('domains', ['name' => 'https://github.com']);
+        $this->seeInDatabase('domains', ['name' => 'https://www.smarty.net',
+                                         'h1' => 'Get Smarty',
+                                         'description' => 'Smarty is a template engine for PHP.',
+                                         'keywords' => 'smarty, template, engine, php'
+                                         ]);
+        $this->seeInDatabase('domains', ['name' => 'https://www.smarty.net']);
     }
     public function testIndex()
     {
-        $this->get('/domains');
+        $this->get(route('domains.index'));
         $this->seeStatusCode(200);
     }
     public function testShow()
     {
-        $this->get('/domains', ['id' => 1]);
-        $this->seeStatusCode(200);
+        $this->post(route('domains.store'), ['url' => 'https://www.smarty.net']);
+        $this->get(route('domains.show', ['id' => 1]));
+        $this->seeInDatabase('domains', ['name' => 'https://www.smarty.net',
+                                         'h1' => 'Get Smarty',
+                                         'description' => 'Smarty is a template engine for PHP.',
+                                         'keywords' => 'smarty, template, engine, php']);
     }
 }
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,6 +1,7 @@
 <?php
 
 use Laravel\Lumen\Testing\TestCase as BaseTestCase;
+use Illuminate\Http\Request;
 
 abstract class TestCase extends BaseTestCase
 {
@@ -11,6 +12,19 @@ abstract class TestCase extends BaseTestCase
      */
     public function createApplication()
     {
-        return require __DIR__ . '/../bootstrap/app.php';
+        $app = require __DIR__.'/../bootstrap/app.php';
+        $uri = $app->make('config')->get('app.url', 'http://localhost');
+        $components = parse_url($uri);
+        $server = $_SERVER;
+        if (isset($components['path'])) {
+            $server = array_merge($server, [
+                'SCRIPT_FILENAME' => $components['path'],
+                'SCRIPT_NAME' => $components['path'],
+            ]);
+        }
+        $app->instance('request', Request::create(
+            $uri, 'GET', [], [], [], $server
+        ));
+        return $app;
     }
 }
