34b790a0e32d3769638bcf5ea1e9c8e74d9f3687
mvaload <mvaload@gmail.com>
2019-09-19 17:41:34 +0300

    added new model factory

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -17,3 +17,9 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'email' => $faker->email,
     ];
 });
+
+$factory->define(App\Domain::class, function (Faker\Generator $faker) {
+	return [
+		'name' => $faker->url
+	];
+});
