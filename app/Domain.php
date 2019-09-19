35cd019dcbd0224abb869097b4a4e92a65d98161
mvaload <mvaload@gmail.com>
2019-09-19 17:26:26 +0300

    updated model

--- app/Domain.php
+++ app/Domain.php
@@ -6,6 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Domain extends Model
 {
-    // public $timestamps = true;
-    protected $fillable = ['name', 'contentLength', 'responseCode', 'body', 'h1', 'keywords', 'description'];
+    protected $fillable = [
+        'name',
+        'contentLength', 
+        'responseCode', 
+        'body', 
+        'h1', 
+        'keywords', 
+        'description'
+    ];
 }
