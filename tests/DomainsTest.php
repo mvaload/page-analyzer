3ea6d9bf069d39c1103d9d447dd8e1146861032f
mvaload <mvaload@gmail.com>
2019-09-19 17:43:10 +0300

    changed tests

--- tests/DomainsTest.php
+++ tests/DomainsTest.php
@@ -1,51 +1,48 @@
 <?php
 
-use Laravel\Lumen\Testing\DatabaseMigrations;
 use Laravel\Lumen\Testing\DatabaseTransactions;
-use App\Http\Controllers\DomainsController;
 use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Response;
 use GuzzleHttp\Handler\MockHandler;
 use GuzzleHttp\HandlerStack;
-use GuzzleHttp\Psr7\Response;
-use GuzzleHttp\Psr7\Request;
-use GuzzleHttp\Exception\RequestException;
-use App\Post;
 
 class DomainsTest extends TestCase
 {
     use DatabaseTransactions;
 
+    protected function setUp():void
+    {
+        parent::setUp();
+        $path = 'tests/fixtures/test.html';
+        $body = file_get_contents($path);
+        $mock = new MockHandler([
+            new Response(200, ['Content-Length' => 11], $body)
+        ]);
+        $handler = HandlerStack::create($mock);
+        $this->app->bind('GuzzleClient', function ($app) use ($handler) {
+            return new Client(['handler' => $handler]);
+        });
+    }
+
     public function testStore()
     {
-        $mockGuzzle = new MockHandler([
-            new Response(200, ['Content-Length' => 0,
-                                'body' => '<h1>Tag</h1>',
-                                'description' => 'description smarty',
-                                'keywords' => 'keywords smarty']),
-            new Response(200, ['body' => '<h1>Tag</h1>',
-                               'description' => 'description smarty',
-                               'keywords' => 'keywords smarty'])
-        ]);
-        $handler = HandlerStack::create($mockGuzzle);
-        $guzzle = new Client(['handler' => $handler]);
-        $this->app->instance(Client::class, $guzzle);
-
-        $this->post(route('domains.store'), ['url' => 'https://www.smarty.net']);
-        $this->seeStatusCode(302);
-        $this->seeInDatabase('domains', ['name' => 'https://www.smarty.net']);
+        $domain = factory('App\Domain')->make();
+        $this->post(route('domains.store'), ['domain' => $domain->name]);
+        $this->assertResponseStatus(302);
+        $this->seeInDatabase('domains', ['name' => 'http://google.com']);
     }
+
     public function testIndex()
     {
         $this->get(route('domains.index'));
         $this->seeStatusCode(200);
     }
+
     public function testShow()
     {
-        $this->post(route('domains.store'), ['url' => 'https://www.smarty.net']);
-        $this->get(route('domains.show', ['id' => 1]));
-        $this->seeInDatabase('domains', ['name' => 'https://www.smarty.net',
-                                         'h1' => 'Get Smarty',
-                                         'description' => 'Smarty is a template engine for PHP.',
-                                         'keywords' => 'smarty, template, engine, php']);
+        $domain = factory('App\Domain')->make();
+        $domain->save();
+        $this->get(route('domains.show', ['id' => $domain->id]));
+        $this->assertResponseStatus(200);
     }
 }
