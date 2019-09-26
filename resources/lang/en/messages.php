864ed288b2eee527e592137b8e645d8e34658a0d
mvaload <mvaload@gmail.com>
2019-09-26 12:05:40 +0300

    added i18n messages

---
+++ resources/lang/en/messages.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'home' => 'Home',
+    'list' => 'Domain List',
+    'title' => 'Page analyzer',
+    'text' => 'Enter URL to start. Example URL: http://example.com',
+    'required' => 'URL entered incorrectly. Please enter valid URL (example - http://example.com)',
+    'unique' => 'URL is already added to database.'
+]; 
