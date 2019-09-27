64a1a0497fb51dcf75ef15e28772e5ec48eff4ba
mvaload <mvaload@gmail.com>
2019-09-27 09:17:41 +0300

    fixed error

--- app/Jobs/DomainJob.php
+++ app/Jobs/DomainJob.php
@@ -23,8 +23,9 @@ class DomainJob extends Job
        
         $document = $this->parse();
         $this->domain->created_at = Carbon::now()->toDateTimeString();
-        $this->domain->contentLength = isset($response->getHeader('Content-Length')[0]) ?
-                                            $response->getHeader('Content-Length')[0] : 0;
+        $this->domain->contentLength = $response->getHeader('Content-Length') ?
+                                        implode('', $response->getHeader('Content-Length')) :
+                                        strlen($response->getBody());
         $this->domain->responseCode = $response->getStatusCode();
         $this->domain->body = $response->getBody()->getContents();
         $this->domain->h1 = $document->has('h1') ? $document->first('h1')->text() : 'undefined';
